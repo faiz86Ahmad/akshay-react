58dcc3d032f6e4b02dbb640e0211c24955cbbadd
faizan ahmad <faizan89ahmad@gmail.com>
2024-02-05 22:50:28 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading = React.createElement("h1",{},"hello world")
+  
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  //passing a react element inside the root
+  
+  //async defer
+  root.render(heading);
